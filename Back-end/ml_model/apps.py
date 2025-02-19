06d9f4ea9dd68104d9174c7efdbc0b5f667b8812
IceBag95 <pan.m.pagonis@gmail.com>
2025-02-19 15:10:43 +0200

    Trying to fix the issue #1

---
+++ Back-end/ml_model/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from ML.train_with_adaboost import train_model
+
+
+class MlModelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ml_model'
+
+    def ready(self):
+        train_model()
