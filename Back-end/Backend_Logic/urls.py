70357d6818667ed46896b91cf9612e3ef63a0209
IceBag95 <pan.m.pagonis@gmail.com>
2025-02-28 14:22:56 +0200

    Made the new url in backend

--- Back-end/Backend_Logic/urls.py
+++ Back-end/Backend_Logic/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from ml_model import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('predict', views.heart_attack_prediction, name='predict'),
 ]
